0c6915f85c976cdc1b530c0fa48e407965efd574
faircoj22 <Jamie@digitalisland.gg>
2024-11-24 22:20:18 +0000

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,24 @@
-window.function = function(id, geo) {
-  const rowID = id.value;
-  const geoValue = geo.value;
-  const result = rowID + geoValue;
+window.function = function(coordinates) {
+  // Initialize the map component if it hasn't been initialized
+  if (!window.mapComponent) {
+    window.mapComponent = {
+      init: function() {
+        if (!window.L) {
+          // Load Leaflet if not already loaded
+          const script = document.createElement('script');
+          script.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+          document.head.appendChild(script);
 
-  return result;
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
+          document.head.appendChild(link);
+        }
+      }
+    };
+    window.mapComponent.init();
+  }
+
+  // Return the coordinates value
+  return coordinates ? coordinates.value : "";
 }
