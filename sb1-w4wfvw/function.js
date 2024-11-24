74346896da44ce35826c730175a21289d4f6e9b9
faircoj22 <Jamie@digitalisland.gg>
2024-11-24 22:10:55 +0000

    Delete sb1-w4wfvw directory

--- sb1-w4wfvw/function.js
+++
@@ -1,7 +0,0 @@
-window.function = function(id, geo) {
-  const rowID = id.value;
-  const geoValue = geo.value;
-  const result = rowID + geoValue;
-
-  return result;
-}
--- sb1-w4wfvw/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { MyGlidePlugin } from './plugin';
